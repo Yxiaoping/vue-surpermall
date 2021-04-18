bdcb4f52c713e6c1cb3fa0f8288eea5d006cd5fe
Elephantx <2509458078@qq.com>
2021-04-18 15:58:56 +0800

    tabbar

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="tab-bar">
+        <slot/>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#tab-bar{
+  display: flex;
+  background: rgb(206, 205, 205);
+  height: 49px;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  box-shadow: 0 -3px 3px rgba(100, 100, 100, 0.2);
+}
+</style>
---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="tab-bar-item" @click="itemClick">
+      <div v-if="!isActive"><slot name="item-icon"></slot></div>
+      <div v-else><slot name="item-icon-active"></slot></div>
+      <div :style="activeStyle"><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: 'red'
+    }
+  },
+  data () {
+    return {
+      // isActive: true
+    }
+  },
+  computed: {
+    isActive () {
+      return this.$route.path.indexOf(this.path) !== -1
+    },
+    activeStyle () {
+      return this.isActive ? {color: this.activeColor} : {}
+    }
+  },
+  methods: {
+    itemClick () {
+      this.$router.push(this.path)
+    }
+  }
+}
+</script>
+
+<style>
+.tab-bar-item{
+  flex: 1;
+  text-align: center;
+  font-size: 14px;
+}
+.tab-bar-item img{
+  width: 24px;
+  height: 24px;
+  vertical-align: middle;
+  margin: 3px 0 2px 0;
+}
+</style>
