e9399f1221ce1d79be5e55fdf6989e3fd32986eb
Elephantx <2509458078@qq.com>
2021-04-18 15:35:18 +0800

    f

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -11,18 +9,10 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/css/base.css'
 </style>
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    configureWebpack: {
+        resove: {
+            alias: {
+                'assets': '@/assets',
+                'common': '@/common',
+                'components': '@/components',
+                'network': '@/network',
+                'views': '@/views'
+            }
+        }
+    }
+}
